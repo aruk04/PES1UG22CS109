277f8a96398ba7ef0e89ec65dbf39b0f7d3100a1
aruk04 <141922774+aruk04@users.noreply.github.com>
2025-03-18 16:40:18 +0530

    Create Jenkinsfile.cpp

---
+++ Jenkinsfile.cpp
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    stages {
+        stage('Clone repository') {
+            steps {
+                checkout([$class: 'GitSCM', 
+                    branches: [[name: 'main']], 
+                    userRemoteConfigs: [[url: 'https://github.com/aruk04/PES1UG22CS109.git']]
+                ])
+            }
+        }
+        stage('Build') {
+            steps {
+                build 'PES1UG22CS109-1'
+                sh 'g++ ./main/hello.cpp -o ./output'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh './output'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'deploy'
+            }
+        }
+    }
+    post {
+        failure {
+            error 'Pipeline failed'
+        }
+    }
+}
